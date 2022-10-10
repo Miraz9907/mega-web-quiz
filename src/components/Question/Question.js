63812f50cab67bcda90844c8abd3f466f27bc46f
Miraz9907 <meraz.rahman99@gmail.com>
2022-10-11 02:51:46 +0600

    single question show in ui

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Question = () => {
+const Question = ({quiz}) => {
+    // console.log(quiz);
+    const {correctAnswer, options, question} = quiz;
     return (
-        <div>
-            <h2>test exam</h2>
+        <div className='border-2 m-4'>
+            <h3>question: {question}</h3>
+            {
+                options.map(op => <li>{op}</li>)
+            }
+            <h2>currect ans: {correctAnswer}</h2>
+
+            
+
         </div>
     );
 };
--- src/components/TestQuiz/TestQuiz.js
+++ src/components/TestQuiz/TestQuiz.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Question from '../Question/Question';
 
 const TestQuiz = () => {
     const allQuestion = useLoaderData();
     const {questions} =allQuestion.data;
-    console.log(questions)
+    // console.log(questions)
     
     return (
         <div>
-            
+            {
+                questions.map(quiz =><Question
+                    key={quiz.id}
+                    quiz= {quiz}
+                    ></Question>)
+            }
         </div>
     );
 };
